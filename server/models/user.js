b0472e60fa0545b6d7aea91a2444daf5add03a0a
Fahad Jabbar <fahadjabbar31@gmail.com>
2020-03-24 21:34:36 +0500

    [PG-0001] Mongo Schema for Employees

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,118 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const employeeSchema = new Schema(
+  {
+    personal_information: {
+      name: String,
+      company_email: {
+        type: String,
+        required: true,
+      },
+      father_name: String,
+      date_of_birth: Date,
+      gender: String,
+      nationality: String,
+      city: {
+        type: Schema.Types.ObjectId,
+        ref: 'City'
+      },
+      country: {
+        type: Schema.Types.ObjectId,
+        ref: 'Country'
+      },
+      religion: String,
+      marital_status: String,
+      blood_group: String,
+      disability: String,
+      cnic: Number,
+      ntn: Number,
+      domicile: Number
+    },
+    official_information: {
+      employee_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Employee'
+      },
+      employee_status: String,
+      designation: String,
+      social_security: Number,
+      health_benefits: [String],
+      provident_fund: Number,
+      department: {
+        type: Schema.Types.ObjectId,
+        ref: 'Department',
+      },
+      joining_date: Date,
+      resignation_date: Date,
+      exit_date: Date
+    },
+    contact_information: [
+      {
+        title: String,
+        type: String,
+        detail: String
+      }
+    ],
+    salary_settings: {
+      mode_of_payment: String,
+      ex_gratia_on_overtime: Number,
+      gratuity: Number,
+      bank_details: String,
+    },
+    academics: [
+      {
+        degree_name: String,
+        board: String,
+        university: String,
+        marks: Number,
+        grade: String,
+      }
+    ],
+    experience: [
+      {
+        organization: String,
+        designation: String,
+        duration: String,
+        leaving_reason: String,
+        salary: Number,
+      },
+    ],
+    dependants: [
+      {
+        name: String,
+        gender: String,
+        relation: String,
+        contact: Number,
+        date_of_birth: Date
+      },
+    ],
+    company_assets: [
+      {
+        name: String,
+        detail: String,
+        returnable: Boolean,
+        status: String,
+        issue_data: Date
+      },
+    ],
+    leave_balance: Number,
+    duties: [
+      {
+        job: String,
+        frequency: String,
+        effective_from: Date,
+        enchanced_till: Date
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-export default User;
+const User = mongoose.model('User', userSchema);
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = {
+  User: User,
+  Employee: Employee
+}
