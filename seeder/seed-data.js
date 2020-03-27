36000d440bca0bb15bf818a24a39d08aa8a2312d
Fahad Jabbar <fahadjabbar31@gmail.com>
2020-03-27 19:51:35 +0500

    [PG-0002] use es6 features, change database

--- seeder/seed-data.js
+++ seeder/seed-data.js
@@ -1,11 +1,10 @@
 
-const MongoClient = require('mongodb').MongoClient;
-// import MONGO_URL from '@config';
-const Config = require('../config');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+import { MongoClient } from 'mongodb';
+import { MONGO_URL } from '@config';
+import values from 'lodash/values';
+import bcrypt from 'bcrypt';
 
-MongoClient.connect(Config.MONGO_URL,
+MongoClient.connect(MONGO_URL,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -15,7 +14,7 @@ MongoClient.connect(Config.MONGO_URL,
             console.log('error while connecting mongoDB atlas ..\n', err);
             return;
         }
-        const db = client.db('test');
+        const db = client.db('development');
 
         await db.collection("permissions").remove();
         await db.collection("roles").remove();
@@ -41,27 +40,24 @@ MongoClient.connect(Config.MONGO_URL,
 
         // create permissions
         let savedPermissions = await db.collection("permissions").insertMany(permissions);
-        let permissionIdsArray = [];
-        Object.keys(savedPermissions.insertedIds).map((key) => {
-            permissionIdsArray.push(savedPermissions.insertedIds[key])
-        })
+        let permissionIds = values(savedPermissions.insertedIds);
 
         let roles = []
-        createRole = (name, description, permissions) => {
+        const createRole = (name, description, permissions) => {
             return {
                 name,
                 description,
                 permissions,
             }
         }
-        roles.push(createRole('ADMIN', '', permissionIdsArray));
-        roles.push(createRole('EMPLOYEE', '', permissionIdsArray));
+        roles.push(createRole('ADMIN', '', permissionIds));
+        roles.push(createRole('EMPLOYEE', '', permissionIds));
 
         // create roles
-        const savedRoles = await db.collection("roles").insertMany(roles);
+        await db.collection("roles").insertMany(roles);
 
         let users = [];
-        createAdmin = (email, role, name, password, isActive, isVerified) => {
+        const createAdmin = (email, role, name, password, isActive, isVerified) => {
             return {
                 email,
                 role,
@@ -75,12 +71,12 @@ MongoClient.connect(Config.MONGO_URL,
 
         users.push(createAdmin('admin@gmail.com', adminRole._id, 'Admin', 'testing', true, true));
         users.push(createAdmin('test@gmail.com', adminRole._id, 'Test', 'testing', true, true));
-        users.push(createAdmin('super@gmail.com', adminRole._id, 'Super', 'testing', true, true));
+        users.push(createAdmin('super@gmail.com', adminRole._id, 'Super Admin', 'testing', true, true));
 
         // create users
         await db.collection("users").insertMany(users, (err, res) => {
             if (err) throw err;
         });
 
-        // client.close();
+        client.close();
     });
