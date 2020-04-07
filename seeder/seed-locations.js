ac50ce4a382381bbdbfe07d7a08bf91f4203515c
Muhammad Musa <musaghauri@hotmail.com>
2020-04-07 18:26:58 +0500

    [PG-0005] Refactored the code

--- seeder/seed-locations.js
+++ seeder/seed-locations.js
@@ -1,122 +1,109 @@
-import { MongoClient } from "mongodb";
-import { MONGO_URL } from "@config";
-import _values from "lodash/values";
-import { countries } from "./Data/countries";
-import { CITIES_OF_PUNJAB } from "./Data/Cities/punjab";
-import { CITIES_OF_SINDH } from "./Data/Cities/sindh";
-import { CITIES_OF_KPK } from "./Data/Cities/kpk";
-import { CITIES_OF_GB } from "./Data/Cities/gb";
-import { CITIES_OF_AJK } from "./Data/Cities/ajk";
-import { CITIES_OF_BALOCHISTAN } from "./Data/Cities/balochistan";
-import { CITIES_OF_ISLAMABAD } from "./Data/Cities/islamabad";
+import { MongoClient } from 'mongodb';
+import { MONGO_URL } from '@config';
+import { countries } from './Data/countries';
+import { CITIES_OF_PUNJAB } from './Data/Cities/punjab';
+import { CITIES_OF_SINDH } from './Data/Cities/sindh';
+import { CITIES_OF_KPK } from './Data/Cities/kpk';
+import { CITIES_OF_GB } from './Data/Cities/gb';
+import { CITIES_OF_AJK } from './Data/Cities/ajk';
+import { CITIES_OF_BALOCHISTAN } from './Data/Cities/balochistan';
+import { CITIES_OF_ISLAMABAD } from './Data/Cities/islamabad';
 
 MongoClient.connect(
   MONGO_URL,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   },
   async (err, client) => {
     if (err) {
-      console.log("error while connecting mongoDB atlas ..\n", err);
+      console.log('error while connecting mongoDB atlas ..\n', err);
       return;
     }
-    const db = client.db("development");
+    const db = client.db('development');
 
-    await db.dropCollection("countries");
-    await db.dropCollection("states");
-    await db.dropCollection("cities");
+    await db.dropCollection('countries');
+    await db.dropCollection('states');
+    await db.dropCollection('cities');
 
-    //countries
-    await db.collection("countries").insertMany(countries);
+    // countries
+    await db.collection('countries').insertMany(countries);
 
-    //States
+    // States
     const pakistan = await db
-      .collection("countries")
-      .findOne({ name: "Pakistan" });
+      .collection('countries')
+      .findOne({ name: 'Pakistan' });
     const states = [
       {
-        name: "Azad Jammu and Kashmir",
+        name: 'Azad Jammu and Kashmir',
         country: pakistan._id,
-        createdBy: null
       },
-      { name: "Balochistan", country: pakistan._id, createdBy: null },
-      { name: "Gilgit Baltistan", country: pakistan._id, createdBy: null },
+      { name: 'Balochistan', country: pakistan._id },
+      { name: 'Gilgit Baltistan', country: pakistan._id },
       {
-        name: "Islamabad Capital Territory",
+        name: 'Islamabad Capital Territory',
         country: pakistan._id,
-        createdBy: null
       },
-      { name: "Khyber Pakhtunkhwa", country: pakistan._id, createdBy: null },
-      { name: "Punjab", country: pakistan._id, createdBy: null },
-      { name: "Sindh", country: pakistan._id, createdBy: null }
+      { name: 'Khyber Pakhtunkhwa', country: pakistan._id },
+      { name: 'Punjab', country: pakistan._id },
+      { name: 'Sindh', country: pakistan._id },
     ];
 
-    await db.collection("states").insertMany(states, (err, res) => {
+    await db.collection('states').insertMany(states, (err, res) => {
       if (err) throw err;
     });
 
-    //cities
+    // cities
     const cities = [];
 
-    const Punjab = await db.collection("states").findOne({ name: "Punjab" });
-    const Sindh = await db.collection("states").findOne({ name: "Sindh" });
+    const Punjab = await db.collection('states').findOne({ name: 'Punjab' });
+    const Sindh = await db.collection('states').findOne({ name: 'Sindh' });
     const KPK = await db
-      .collection("states")
-      .findOne({ name: "Khyber Pakhtunkhwa" });
+      .collection('states')
+      .findOne({ name: 'Khyber Pakhtunkhwa' });
     const Islamabad = await db
-      .collection("states")
-      .findOne({ name: "Islamabad Capital Territory" });
+      .collection('states')
+      .findOne({ name: 'Islamabad Capital Territory' });
     const GB = await db
-      .collection("states")
-      .findOne({ name: "Gilgit Baltistan" });
+      .collection('states')
+      .findOne({ name: 'Gilgit Baltistan' });
     const Balochistan = await db
-      .collection("states")
-      .findOne({ name: "Balochistan" });
+      .collection('states')
+      .findOne({ name: 'Balochistan' });
     const AJK = await db
-      .collection("states")
-      .findOne({ name: "Azad Jammu and Kashmir" });
+      .collection('states')
+      .findOne({ name: 'Azad Jammu and Kashmir' });
     cities.push(
       ...CITIES_OF_PUNJAB.map(city => ({
         city,
         state: Punjab._id,
-        createBy: null
       }))
     );
     cities.push(
       ...CITIES_OF_SINDH.map(city => ({
         city,
         state: Sindh._id,
-        createBy: null
       }))
     );
-    cities.push(
-      ...CITIES_OF_KPK.map(city => ({ city, state: KPK._id, createBy: null }))
-    );
+    cities.push(...CITIES_OF_KPK.map(city => ({ city, state: KPK._id })));
     cities.push(
       ...CITIES_OF_ISLAMABAD.map(city => ({
         city,
         state: Islamabad._id,
-        createBy: null
       }))
     );
-    cities.push(
-      ...CITIES_OF_GB.map(city => ({ city, state: GB._id, createBy: null }))
-    );
+    cities.push(...CITIES_OF_GB.map(city => ({ city, state: GB._id })));
     cities.push(
       ...CITIES_OF_BALOCHISTAN.map(city => ({
         city,
         state: Balochistan._id,
-        createBy: null
       }))
     );
-    cities.push(
-      ...CITIES_OF_AJK.map(city => ({ city, state: AJK._id, createBy: null }))
-    );
+    cities.push(...CITIES_OF_AJK.map(city => ({ city, state: AJK._id })));
 
-    await db.collection("cities").insertMany(cities, (err, res) => {
+    await db.collection('cities').insertMany(cities, (err, res) => {
       if (err) throw err;
-      console.log("seeding completed");
+      console.log('seeding completed');
     });
 
     client.close();
