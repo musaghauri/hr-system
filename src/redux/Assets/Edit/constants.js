5503c8af0393aee264b100cdb22badd597361db9
hereisfahad <fahadjabbar31@gmail.com>
2020-04-21 23:45:20 +0500

    remove typo from edit constants

--- src/redux/Assets/Edit/constants.js
+++ src/redux/Assets/Edit/constants.js
@@ -5,10 +5,10 @@ export const EDIT_ASSET = 'HRMS/Assets/Edit/EDIT_ASSET';
 export const EDIT_ASSET_SUCCESS =
   'HRMS/Assets/Edit/EDIT_ASSET_SUCCESS';
 export const EDIT_ASSET_FAIL =
-  'HRMS/Assets/Edit/gin/EDIT_ASSET_FAIL';
+  'HRMS/Assets/Edit/EDIT_ASSET_FAIL';
 
 export const GET_ASSET = 'HRMS/Assets/Edit/GET_ASSET';
 export const GET_ASSET_SUCCESS =
   'HRMS/Assets/Edit/GET_ASSET_SUCCESS';
 export const GET_ASSET_FAIL =
-  'HRMS/Assets/Edit/gin/GET_ASSET_FAIL';
+  'HRMS/Assets/Edit/GET_ASSET_FAIL';
