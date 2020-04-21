5be8a786b8d5e80512f82519ee9dec3f3c04d7f7
Muhammad Musa <musaghauri@hotmail.com>
2020-04-22 03:57:49 +0500

    [PG-0027] Removed ifAttributeExists function from annoucement helper

--- server/helpers/Announcement.js
+++ server/helpers/Announcement.js
@@ -1,11 +1,9 @@
 /* eslint-disable */
-import httpStatus from 'http-status';
 import Announcement from '../models/announcement';
 import _startCase from 'lodash/startCase';
 import _get from 'lodash/get';
 import MongoHanlder from './common/mongo';
 import CrudHandler from './common/crud';
-import APIError from './common/APIError';
 
 const mongoDriver = new MongoHanlder(Announcement);
 const activeDriver = mongoDriver;
@@ -32,48 +30,11 @@ function Get(params, populate= ['','']) {
 }
 
 function UpdateById(announcementId, announcementBody, user = null) {
-  return new Promise((resolve, reject) => {
-    const {
-      title = null,
-    } = announcementBody;
-    ifAttributeExists({ title }, title, announcementId).then(data => {
-      if(!!(_get(data, 'status'))) {
-        reject({
-          status: httpStatus.BAD_REQUEST,
-          err: _get(data, 'err'),
-        });
-      } else {
-        CRUD.updateById(announcementId, announcementBody, user).then(updatedAnnouncement => {
-          resolve(updatedAnnouncement);
-        })
-        .catch(e => reject(e));
-      }
-    })
-    .catch(e => reject(e));
-  });
+  return CRUD.updateById(announcementId, announcementBody, user);
 }
 
 function Create(announcementBody, user = null) {
-  return new Promise((resolve, reject) => {
-    const {
-      title = null,
-    } = announcementBody;
-    
-    ifAttributeExists({ title }, title).then(data => {
-      if(!!(_get(data, 'status'))) {
-        reject({
-          status: httpStatus.BAD_REQUEST,
-          err: _get(data, 'err'),
-        });
-      } else {
-        CRUD.create(Announcement, announcementBody, user).then(savedAnnouncement => {
-          resolve(savedAnnouncement);
-        })
-        .catch(e => reject(e));
-      }
-    })
-    .catch(e => reject(e));
-  });
+  return CRUD.create(Announcement, announcementBody, user);
 }
 
 
@@ -81,25 +42,5 @@ function Remove(id, user = null) {
   return CRUD.remove(id, user);
 }
 
-
-
-function ifAttributeExists(values, key, exception = null) {
-  return new Promise((resolve, reject) => {
-    CRUD.isUnique(values, exception).then((valueExists) => {
-      if (!!valueExists) {
-        const err = new APIError(`${_startCase(key)} already exists`, httpStatus.BAD_REQUEST);
-        resolve({
-          status: true,
-          err,
-        });
-      } else {
-        resolve({
-            status: false
-        });
-      }
-    })
-    .catch(e => reject(e));
-  });
-}
 
 export default { Create, GetOne, UpdateById, GetById, Get, Remove };
