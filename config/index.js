55c5408cd0522f02e8d45b107a3cb039a73ef79c
Muhammad Musa <musaghauri@hotmail.com>
2020-09-15 18:07:47 +0500

    chore: Removed mongo uri

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const NEXT_API_URL = PROD
   ? 'https://bitbytes-hrms.herokuapp.com/api'
   : 'http://localhost:8000/api';
 const MONGO_URL =
-  'mongodb+srv://bitbytes:bitbytes123@hr-system-5a8n4.mongodb.net/development?retryWrites=true&w=majority';
+  '';
 const { NODE_ENV } = process.env;
 const AUTH_COOKIE_NAME = 'hrms';
 const ROLE_OPTIONS = [
